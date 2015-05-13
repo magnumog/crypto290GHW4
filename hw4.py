ec63a0eb3e1cdbf56dcaceef0f908d31b4fcd2e1
Magnus Settemsli Mogstad <magnumog@stud.ntnu.no>
2015-05-13 14:02:50 -0700

    foobar

--- hw4.py
+++ hw4.py
@@ -13,6 +13,13 @@ def fermat(n):
             liar.add(n)
     return witness, liar
 
+def millerRabin(n):
+    witness = []
+    liar = []
+    for i in range(,len(n)):
+        a = random.randint(1,n[i]-1)
+        x = a**m%n[i]
+
 int numbers = {41041, 62745, 63973, 75361, 101101, 126217, 172081, 188461, 278545, 340561, 449065, 552721, 656601, 658801, 670033, 748657, 838201, 852841, 997633, 1033669, 1082809, 1569457, 1773289, 2100901, 2113921, 2433601, 2455921}
 
 print(fermat(numbers))
