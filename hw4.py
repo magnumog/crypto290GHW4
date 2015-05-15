5b2257bd12fdf1e10e1d53661fe51705e71f5074
magnumog <magnumog@stud.ntnu.no>
2015-05-14 22:45:06 -0700

    Should be done

--- hw4.py
+++ hw4.py
@@ -1,40 +1,41 @@
 import random
 import math
 
-def fermat(n):
-    #print('n',n)
-    witness = 0
-    liar = 0
-    #a = random.randint(2,n-2)
-    for a in range(2,int(math.sqrt(n))):
-        x = (a**(n-1))%n
-        #print('x',x)
-        if(x!=(1%n)):
-            if(witness==0 or x<witness):
-                witness = x
-        else:
-            if(liar==0 or x<liar):
-                liar = x
-    return witness, liar
+def fermat(n,a):
+    x = fastermod(a,(n-1),n)
+    if(x!=(1%n)):
+        return True
+    return False
 
-def millerRabin(n):
-    witness = 0
-    liar = 0
-    k,m=decompose(n)
-    a = random.randint(1,n-1)
-    x = (a**m)%n
-    if((x==1 and (witness==0 or witness>x)):
-        witness=x
-        if(witness==0):
-            for j in range(0,k-1):
-                if(x==-1%n and (witness==0 or witness>x)):
-                    witness=x**2%n
-    elif(witness==0 and x<liar):
-        liar = x
-    return witness, liar
+def millerRabin(n,a):
+    k,m =  decompose(n)
+    x = fastermod(a,m,n)
+    if(x==1%n):
+        return False
+    for j in range(0,k-1):
+        if(x==-1%n):
+            return False
+        else:
+            x = fastermod(x,2,n)
+    return True
 
 def decompose(n):
-    return 0
+    exponentOfTwo = 0
+    while n % 2 == 0:
+        n = n/2
+        exponentOfTwo += 1
+    return exponentOfTwo, n
+
+def fastermod(factor,power,modulus):
+    result = 1;
+    while(power > 0):
+        if (power % 2 == 1):    
+            result = (result*factor) % modulus
+            power = power-1
+
+        power = power/2;
+        factor = (factor*factor)%modulus
+    return result
 
 
 numbers = []
@@ -66,12 +67,57 @@ numbers.append(2113921)
 numbers.append(2433601)
 numbers.append(2455921)
 
-def main():
-    #witness, liar = fermat(143)
-    for i in range(0, len(numbers)):
-        witness, liar = fermat(numbers[i])
-        print("witness", int(witness))
-        print("liar", int(liar))
+def runFermat():
+    witnesses = []
+    liars = []
+    #for i in range(0,len(numbers)):
+    for i in range(0,len(numbers)):
+        tempWitness=0
+        tempLiar=0
+        for a in range(2,int(numbers[i])):
+            bol = fermat(numbers[i],a)
+            if(bol and tempWitness==0):
+                tempWitness=a
+            elif(not(bol) and tempLiar==0):
+                tempLiar=a
+            if(tempLiar!=0 and tempWitness!=0):
+                break
+        witnesses.append(int(tempWitness))
+        liars.append(int(tempLiar))
+    #print("Witnesses",witnesses,len(witnesses))
+    #print("liars",liars,len(liars))
+    print "Fermat"
+    printNice(witnesses,liars)
 
-def foo():
-    print(math.sqrt(numbers[0]))
+
+def runMillerRabin():
+
+    witnesses = []
+    liars = []
+    for i in range(0,len(numbers)):
+        tempLiar=0
+        tempWitness=0
+        for a in range(1,int(numbers[i])):
+            bol = millerRabin(numbers[i],a)
+            if(bol and tempWitness==0):
+                tempWitness=a
+            elif(not(bol) and tempLiar==0):
+                tempLiar=a
+            if(tempLiar!=0 and tempWitness!=0):
+                break
+        witnesses.append(int(tempWitness))
+        liars.append(int(tempLiar))
+    #print("witnesses",witnesses,len(witnesses))
+    #print("liars",liars,len(liars))
+    print "Miller-Rabin"
+    printNice(witnesses,liars)
+
+def printNice(witnesses,liars):
+    for i in range(0,len(numbers)):
+        print "NUMBER: ",numbers[i] 
+        print "Witnes: ", witnesses[i]
+        print "Liar: ",liars[i]
+
+def main():
+    runFermat()
+    runMillerRabin()
