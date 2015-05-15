2c61d7459313d27f0bb3a6ce49cde2d46b0021fe
magnumog <magnumog@stud.ntnu.no>
2015-05-14 17:23:20 -0700

    geting some where but dont kno where

--- hw4.py
+++ hw4.py
@@ -1,26 +1,77 @@
 import random
+import math
 
 def fermat(n):
-    witness = []
-    liar = []
-    for i in range(0,len(n)):
-        print(n[i])
-        a = random.randint(2,n[i]-2)
-        x = (a**(n[i]-1))%n
+    #print('n',n)
+    witness = 0
+    liar = 0
+    #a = random.randint(2,n-2)
+    for a in range(2,int(math.sqrt(n))):
+        x = (a**(n-1))%n
+        #print('x',x)
         if(x!=(1%n)):
-            witness.add(n)
+            if(witness==0 or x<witness):
+                witness = x
         else:
-            liar.add(n)
+            if(liar==0 or x<liar):
+                liar = x
     return witness, liar
 
 def millerRabin(n):
-    witness = []
-    liar = []
-    for i in range(,len(n)):
-        a = random.randint(1,n[i]-1)
-        x = a**m%n[i]
+    witness = 0
+    liar = 0
+    k,m=decompose(n)
+    a = random.randint(1,n-1)
+    x = (a**m)%n
+    if((x==1 and (witness==0 or witness>x)):
+        witness=x
+        if(witness==0):
+            for j in range(0,k-1):
+                if(x==-1%n and (witness==0 or witness>x)):
+                    witness=x**2%n
+    elif(witness==0 and x<liar):
+        liar = x
+    return witness, liar
 
-int numbers = {41041, 62745, 63973, 75361, 101101, 126217, 172081, 188461, 278545, 340561, 449065, 552721, 656601, 658801, 670033, 748657, 838201, 852841, 997633, 1033669, 1082809, 1569457, 1773289, 2100901, 2113921, 2433601, 2455921}
+def decompose(n):
+    return 0
 
-print(fermat(numbers))
-       
+
+numbers = []
+numbers.append(41041)
+numbers.append(62745)
+numbers.append(63973)
+numbers.append(75361)
+numbers.append(101101)
+numbers.append(126217)
+numbers.append(172081)
+numbers.append(188461)
+numbers.append(278545)
+numbers.append(340561)
+numbers.append(449065)
+numbers.append(552721)
+numbers.append(656601)
+numbers.append(658801)
+numbers.append(670033)
+numbers.append(748657)
+numbers.append(838201)
+numbers.append(852841)
+numbers.append(997633)
+numbers.append(1033669)
+numbers.append(1082809)
+numbers.append(1569457)
+numbers.append(1773289)
+numbers.append(2100901)
+numbers.append(2113921)
+numbers.append(2433601)
+numbers.append(2455921)
+
+def main():
+    #witness, liar = fermat(143)
+    for i in range(0, len(numbers)):
+        witness, liar = fermat(numbers[i])
+        print("witness", int(witness))
+        print("liar", int(liar))
+
+def foo():
+    print(math.sqrt(numbers[0]))
